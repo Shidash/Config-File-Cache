f86c5dc52d4f9915561795d27f9f8f846dadc17e
Debian Live user <shidash@shidash.com>
2014-11-16 01:05:04 +0000

    Updated setup and rvm config

--- Shidash/setup.sh
+++ Shidash/setup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash 
 
-cp -r /home/amnesia/Persistent/Config-File-Cache/Shidash/utilities /home/amnesia/Persistent/Config-File-Cache/backup /home/amnesia/Persistent/Config-File-Cache/boot.sh /home/amnesia/Persistent # Move scripts to persistent
+cp -r /home/amnesia/Persistent/Config-File-Cache/Shidash/utilities /home/amnesia/Persistent/Config-File-Cache/Shidash/backup /home/amnesia/Persistent/Config-File-Cache/Shidash/boot.sh /home/amnesia/Persistent # Move scripts to persistent
 sudo cp -r /home/amnesia/Persistent/Config-File-Cache/Shidash/dotfiles /live/persistence/TailsData_unlocked # Transfer config files
 sudo chown -R amnesia /live/persistence/TailsData_unlocked/dotfiles
+mkdir /home/amnesia/Persistent/mnt # Make dir for mount flash drive
 sudo /home/amnesia/Persistent/utilities/mount.sh # Transfer key files
 sudo cp -r /home/amnesia/Persistent/mnt/Keyfiles/* /live/persistence/TailsData_unlocked
 sudo chown -R amnesia /live/persistence/TailsData_unlocked/gnupg /live/persistence/TailsData_unlocked/icedove /live/persistence/TailsData_unlocked/openssh-client /live/persistence/TailsData_unlocked/pidgin
-sudo /home/amnesia/Persistent/Utilities/unmount.sh
+sudo /home/amnesia/Persistent/utilities/unmount.sh
+sudo apt-get update
 sudo apt-get install make libssl-dev # Dev env setup
 echo 'rvm_path="$HOME/Persistent/.rvm"' >> ~/.rvmrc
 \curl -sSL get.rvm.io | bash -s stable
@@ -16,8 +18,9 @@ rvm install 2.1.3
 rvm use 2.1.3
 gem install tzinfo
 gem install rails -v 4.1.7
-gem install nokogiri
 curl -L -O https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.3.4.zip
-unzip elasticsearch-1.3.4.zip
-mkdir /home/amnesia/Persistent/mnt # Make dir for mount flash drive
+mv elasticsearch-1.3.4.zip /home/amnesia/Persistent
+unzip /home/amnesia/Persistent/elasticsearch-1.3.4.zip
+rm /home/amnesia/Persistent/elasticsearch-1.3.4.zip
 /home/amnesia/Persistent/boot.sh # Run boot config script
+rm /home/amnesia/.icedove
