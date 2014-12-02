3b18bc0d5523a239c4e0f766f15c5d2dc53336ba
Debian Live user <shidash@shidash.com>
2014-12-02 12:49:40 +0000

    Added packages for VPN

--- Shidash/boot.sh
+++ Shidash/boot.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
 
+source /home/amnesia/Persistent/.rvm/scripts/rvm
 gsettings set org.gnome.desktop.background primary-color '#000000'
 gsettings set org.gnome.desktop.background picture-uri ''
-sudo apt-get install icedove iceowl-extension ghc6 xmonad xmobar dmenu trayer xfce4-terminal libghc-xmonad-contrib-dev xscreensaver emacs23-nox make libssl-dev tahoe-lafs nodejs build-essential
+sudo apt-get install icedove iceowl-extension ghc6 xmonad xmobar dmenu trayer xfce4-terminal libghc-xmonad-contrib-dev xscreensaver emacs23-nox make libssl-dev tahoe-lafs nodejs build-essential network-manager-openvpn network-manager-pptp network-manager-pptp-gnome network-manager-vpnc
 sudo sed -i "s/$outerface lo {/$outerface lo {\n\t\tdaddr 127.0.0.1 proto tcp syn dport 3000 {\n\t\t\tmod owner uid-owner amnesia ACCEPT;\n\t\t}\n\t\tdaddr 127.0.0.1 proto tcp syn dport 8000 {\n\t\t\tmod owner uid-owner amnesia ACCEPT;\n\t\t}\n\t\tdaddr 127.0.0.1 proto tcp syn dport 9200 {\n\t\t\tmod owner uid-owner amnesia ACCEPT;\n\t\t}\n\t\tdaddr 127.0.0.1 proto tcp syn dport 9300 {\n\t\t\tmod owner uid-owner amnesia ACCEPT;\n\t\t}\n\t\tdaddr 127.0.0.1 proto tcp syn dport 9001 {\n\t\t\tmod owner uid-owner amnesia ACCEPT;\n\t\t}\n/" /etc/ferm/ferm.conf
 sudo service ferm restart
-source ~/Persistent/.rvm/scripts/rvm
 killall metacity nautilus gnome-shell gnome-panel
 pkill -9 gnome-panel
 xscreensaver &
