3969cbd47979eb3498906dc5c6c63f4cfadcd26b
Debian Live user <shidash@shidash.com>
2015-04-02 19:24:07 +0000

    Removed mnemosyne stuff (back to using Anki)

--- Shidash/boot.sh
+++ Shidash/boot.sh
@@ -3,7 +3,7 @@
 source /home/amnesia/Persistent/.rvm/scripts/rvm
 gsettings set org.gnome.desktop.background primary-color '#000000'
 gsettings set org.gnome.desktop.background picture-uri ''
-sudo apt-get install icedove iceowl-extension ghc6 xmonad xmobar dmenu trayer xfce4-terminal libghc-xmonad-contrib-dev xscreensaver emacs23-nox make libssl-dev tahoe-lafs nodejs build-essential mnemosyne jmtpfs golang-mode golang
+sudo apt-get install icedove iceowl-extension ghc6 xmonad xmobar dmenu trayer xfce4-terminal libghc-xmonad-contrib-dev xscreensaver emacs23-nox make libssl-dev tahoe-lafs nodejs build-essential golang-mode golang
 sudo sed -i "s/$outerface lo {/$outerface lo {\n\t\tdaddr 127.0.0.1 proto tcp syn dport 3000 {\n\t\t\tmod owner uid-owner amnesia ACCEPT;\n\t\t}\n\t\tdaddr 127.0.0.1 proto tcp syn dport 8000 {\n\t\t\tmod owner uid-owner amnesia ACCEPT;\n\t\t}\n\t\tdaddr 127.0.0.1 proto tcp syn dport 9200 {\n\t\t\tmod owner uid-owner amnesia ACCEPT;\n\t\t}\n\t\tdaddr 127.0.0.1 proto tcp syn dport 9300 {\n\t\t\tmod owner uid-owner amnesia ACCEPT;\n\t\t}\n\t\tdaddr 127.0.0.1 proto tcp syn dport 9001 {\n\t\t\tmod owner uid-owner amnesia ACCEPT;\n\t\t}\n/" /etc/ferm/ferm.conf
 sudo service ferm restart
 killall metacity nautilus gnome-shell gnome-panel
